157a78117a87e153d4d85aa8e5e4652b55743120
jks0522 <wjdrhkdtmd1@gmail.com>
2021-03-24 10:45:22 +0900

    P1541

---
+++ BOJ/P1541/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+
+namespace P1541
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] s = Console.ReadLine().Split('-');
+            int sum = 0;
+
+
+            for(int i = 0; i < s.Length; i++)
+            {
+                if(s[i].Contains("+"))
+                {
+                    string[] arr = s[i].Split('+');
+                    foreach(string sPlus in arr)
+                    {
+                        if (i == 0)
+                            sum += int.Parse(sPlus);
+                        else
+                            sum -= int.Parse(sPlus);
+                    }
+                }
+                else if ( i ==0)
+                {
+                    sum += int.Parse(s[i]);
+                }
+                else
+                {
+                    sum -= int.Parse(s[i]);
+                }
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
