5716e25562e1ab3daff6ee518fb3d1a94b8dd993
jks0522 <wjdrhkdtmd1@gmail.com>
2021-03-25 00:10:52 +0900

    Problem 5086

---
+++ BOJ/P5086/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Text;
+using System.IO;
+
+namespace P5086
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader sr = new StreamReader(Console.OpenStandardInput());
+            StringBuilder sb = new StringBuilder();
+
+            while(true)
+            {
+                string[] s = sr.ReadLine().Split(' ');
+                int num1 = int.Parse(s[0]);
+                int num2 = int.Parse(s[1]);
+                if (num1 == 0 && num2 == 0)
+                    break;
+
+                if (num2 % num1 == 0 && num1 < num2)
+                    sb.AppendLine("factor");
+                else if (num1 % num2 == 0 && num1 > num2)
+                    sb.AppendLine("multiple");
+                else
+                    sb.AppendLine("neither");
+            }
+
+            Console.WriteLine(sb);
+        }
+    }
+}
