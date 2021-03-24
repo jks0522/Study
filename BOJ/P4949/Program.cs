701e3d09eaca54c1a935d4e9deba0fb04a1ba831
jks0522 <wjdrhkdtmd1@gmail.com>
2021-03-24 10:00:06 +0900

    first

---
+++ BOJ/P4949/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Collections;
+
+namespace P4949
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader sr = new StreamReader(Console.OpenStandardInput());
+            StringBuilder sb = new StringBuilder();
+
+
+            while (true)
+            {
+                Stack stack = new Stack();
+                bool isBal = true;
+                string s = sr.ReadLine();
+                if (s == ".")
+                    break;
+
+                for (int i = 0; i < s.Length; i++)
+                {
+                    if (s[i] == '(')
+                        stack.Push('(');
+                    else if (s[i] == '[')
+                        stack.Push('[');
+                    else if (s[i] == ')')
+                    {
+                        if (stack.Count >= 1 && (char)stack.Peek() == '(')
+                            stack.Pop();
+                        else
+                        {
+                            isBal = false;
+                            break;
+                        }
+                    }
+
+                    else if (s[i] == ']')
+                    {
+                        if (stack.Count >= 1 && (char)stack.Peek() == '[')
+                            stack.Pop();
+                        else
+                        {
+                            isBal = false;
+                            break;
+                        }
+
+                    }  
+                }
+
+                if (stack.Count != 0)
+                    isBal = false;
+                if (isBal)
+                    sb.Append("yes\n");
+                else
+                    sb.Append("no\n");
+            }
+
+            Console.WriteLine(sb);
+        }
+    }
+}
