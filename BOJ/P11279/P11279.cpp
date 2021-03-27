091237164556999080fdfbce294673ab738ec40b
jks0522 <wjdrhkdtmd1@gmail.com>
2021-03-27 21:02:58 +0900

    Problem 11279

---
+++ BOJ/P11279/P11279.cpp
@@ -0,0 +1,30 @@
+#include <cstdio>
+#include <queue>
+
+using namespace std;
+
+int main()
+{
+    int n = 0;
+    priority_queue<int> pq;
+
+    scanf("%d", &n);
+    for(int i = 0; i < n; i++)
+    {
+        int x = 0;
+        scanf("%d", &x);
+        
+        if(x == 0)
+        {
+            if(pq.empty())
+                printf("0");
+            else {
+                printf("%d", pq.top());
+                pq.pop();
+            }
+            printf("\n");
+        } else {
+            pq.push(x);
+        }
+    }
+}
