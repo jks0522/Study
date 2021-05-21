fa0889ba1e65b947e165ac9a02ae35bce82d5637
jks0522 <wjdrhkdtmd1@gmail.com>
2021-05-21 09:51:42 +0900

    P1992 쿼드트리

--- BOJ/P1992.cpp
+++ BOJ/P1992.cpp
@@ -1,3 +1,46 @@
 #include <iostream>
-#include <vector>
 using namespace std;
+
+int video[64][64];
+
+string solve(int y, int x, int size) {
+    int head = video[y][x];
+    bool needDivide = false;
+    for(int i = y; i < y+size; i++) {
+        for(int j = x; j < x+size; j++) {
+            if(video[i][j] != head) {
+                needDivide = true;
+                break;
+            }
+        }
+        if(needDivide)
+            break;
+    }
+    int half = size/2;
+
+    if(needDivide) {
+        return "(" + solve(y, x, half) + solve(y, x+half, half) + solve(y+half, x, half)
+            + solve(y+half, x+half, half) + ")";
+    } else {
+        if(head)
+            return "1";
+        else
+            return "0"; 
+    }
+}
+
+int main(void) {
+    int n;  // 영상의 크기
+    cin >> n;
+
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n; j++) {
+            scanf("%1d", &video[i][j]);
+        }
+    }
+
+    string answer = solve(0, 0, n);
+    cout<<answer<<"\n";
+
+    return 0;
+}
