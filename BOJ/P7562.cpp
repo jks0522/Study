32f97085c807ad43d122d5bac440f771a7dd0d9d
jks0522 <wjdrhkdtmd1@gmail.com>
2021-05-26 17:08:51 +0900

    P7562 나이트의 이동

--- BOJ/P7562.cpp
+++ BOJ/P7562.cpp
@@ -5,18 +5,34 @@ using namespace std;
 
 queue<pair<int, int>> q;
 
-int x1, y1, x2, y2;
+int x4, y4, x2, y2;
+
+int dx[] = {1, 2, 2, 1, -1, -2, -2, -1};
+int dy[] = {2, 1, -1, -2, -2, -1, 1, 2};
 
 int bfs(vector<vector<int>> visit) {
-    visit[y1][x1] = 1;
-    q.push({y1, x1});
+    visit[y4][x4] = 1;
+    q.push({y4, x4});
 
     while(!q.empty()) {
         int y = q.front().first;
         int x = q.front().second;
         q.pop();
 
-        
+        if(y == y2 && x == x2) {
+            while(!q.empty())
+                q.pop();
+            return visit[y][x]-1;
+        }
+
+        for(int i = 0; i < 8; i++) {
+            int y3 = y + dy[i];
+            int x3 = x + dx[i];
+            if(y3 >= 0 && y3 < visit.size() && x3 >= 0 && x3 < visit[0].size() && visit[y3][x3] == 0 ) {
+                q.push({y3, x3});
+                visit[y3][x3] = visit[y][x] + 1;
+            }
+        }
     }
 }
 
@@ -32,7 +48,7 @@ int main(void) {
         cin >> l;
         vector<vector<int>> visit(l, vector<int>(l, 0));
 
-        cin >> x1 >> y1 >> x2 >> y2;
+        cin >> x4 >> y4 >> x2 >> y2;
         int answer = bfs(visit);
         cout<<answer<<"\n";
     }
