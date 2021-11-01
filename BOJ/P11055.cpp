e8926ae4c2067e8a17b984146cdeae5d1409abc0
jks0522 <wjdrhkdtmd1@gmail.com>
2021-11-01 14:00:01 +0900

    P11055 가장 큰 증가 부분 수열

---
+++ BOJ/P11055.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <string.h>
+using namespace std;
+
+int n;
+int cache[1001], A[1000];
+
+int lis(int start)
+{
+    int& ret = cache[start+1];
+    if(ret != -1) return ret;
+    ret = 0;
+    for(int next = start+1; next < n; next++)
+        if(start == -1 || A[start] < A[next])
+        {
+            ret = max(ret, lis(next) + A[next]);
+        }
+
+    return ret;
+}
+
+int main(void)
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    memset(cache, -1, sizeof(cache));
+
+    cin >> n;
+    for(int i = 0; i < n; i++)
+        cin >> A[i];
+
+    cout<<lis(-1)<<'\n';
+
+    return 0;
+}
