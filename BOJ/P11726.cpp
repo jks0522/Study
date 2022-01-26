a4a39ebc62c76c5bc220a235b1dd8048b76821a9
jks0522 <wjdrhkdtmd1@gmail.com>
2022-01-27 00:33:16 +0900

    P11726 2xn 타일링

---
+++ BOJ/P11726.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int cache[1001];
+
+int tiling(int n)
+{
+    if(n == 2) return 2;
+    if(n == 1) return 1;
+    if(n <= 0) return 0;
+    int& ret = cache[n];
+    if(ret != 0) return ret;
+
+    ret = tiling(n-2) + tiling(n-1);
+    return ret;
+}
+
+int main(void)
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n;
+    cin >> n;
+
+    cout << tiling(n) << '\n';
+}
